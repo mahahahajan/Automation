989237993e041b7afd1038b778cfb22e762ce1da
mahahahajan <pulkit8.mahajan@gmail.com>
2019-06-10 14:56:36 -0700

    First commit

--- createGitRepo.py
+++ createGitRepo.py
@@ -2,8 +2,8 @@ import sys
 import os
 from github import Github
 
-username = "mahahahajan" #Insert your github username here
-password = "vrpm2281" #Insert your github password here
+username = "" #Insert your github username here
+password = "" #Insert your github password here
 
 def create():
     folderName = str(sys.argv[1])
